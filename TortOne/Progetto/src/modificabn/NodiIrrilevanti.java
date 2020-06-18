f23cdc0792317cd95cddd6948c8d6168048b309b
Luca Gerbaudo <luca.gerbaudo528@edu.unito.it>
2020-06-18 15:50:51 +0200

    Update NodiIrrilevanti.java
    Aggiunti try catch

--- TortOne/Progetto/src/modificabn/NodiIrrilevanti.java
+++ TortOne/Progetto/src/modificabn/NodiIrrilevanti.java
@@ -25,84 +25,97 @@ import probability.bayes.impl.FullCPTNode;
 import probability.proposition.AssignmentProposition;
 
 /**
- *
  * @author davide
  */
 public class NodiIrrilevanti {
     static List<RandomVariable> ancestors = new ArrayList<>();
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int aus = 0;
         HashMap<String, RandomVariable> rvsmap = new HashMap<>();
-        
-        BayesianNetwork bn = BifReader.readBIF("earthquake.xml");
+
+        BayesianNetwork bn = BifReader.readBIF("asia.xml");
         List<RandomVariable> rvs = bn.getVariablesInTopologicalOrder();
-        for (RandomVariable rv :rvs) {
+        for (RandomVariable rv : rvs) {
             System.out.println(rv.getName());
-            rvsmap.put(rv.getName(), rv);       
-        }        
-                
+            rvsmap.put(rv.getName(), rv);
+        }
+
         RandomVariable[] qrv = new RandomVariable[1];
-        qrv[0] = rvsmap.get("JohnCalls");
-        
+        qrv[0] = rvsmap.get("Lung");
+
+        System.out.println("VAL RVSMAP: " + rvsmap.toString());
+
         AssignmentProposition[] ap = new AssignmentProposition[1];
-        ap[0] = new AssignmentProposition(rvsmap.get("Alarm"), "True");
-        
+        ap[0] = new AssignmentProposition(rvsmap.get("Tub"), "Yes");
+
         /*********************************************** Nodi irrilevanti ********************************************/
-        
+
         List<RandomVariable> irrelevants = new ArrayList<>();
-        for(RandomVariable v : bn.getVariablesInTopologicalOrder())
-        {
-            if(isIrrelevant(bn, v, qrv, ap)) {
+        for (RandomVariable v : bn.getVariablesInTopologicalOrder()) {
+            if (isIrrelevant(bn, v, qrv, ap)) {
                 irrelevants.add(v);
-                System.out.println("IRRELEVANT_1 : " + v.getName());
-                
+                System.out.println("IRRELEVANT_1 : " + v.getName() + " ****** " + aus);
+                aus++;
+
                 //Elimino il collegamento variabile irrilevante <-- padri
-                for(Node parent : bn.getNode(v).getParents()) {
+                for (Node parent : bn.getNode(v).getParents()) {
                     parent.removeChildren(bn.getNode(v));
                 }
             }
         }
-                
+
         /***************************************** Nodi irrilevanti Moral Graph *****************************/
 
         Set<RandomVariable> queries = Set.of(qrv);
         Set<RandomVariable> evidences = new HashSet<>();
-        for(AssignmentProposition e : ap)
+        for (AssignmentProposition e : ap)
             evidences.add(e.getTermVariable());
         Set<RandomVariable> nodiIrrilevanti2 = irrilevantRandomVariable(bn, queries, evidences);
-        
+
         List<RandomVariable> nuoveCPT = new ArrayList<>();
-        
+
         //Creo evidenza per le var irrilevanti
-        AssignmentProposition[] ap2 = new AssignmentProposition[nodiIrrilevanti2.size()*2];
-        int k=0;
-        for(RandomVariable v : nodiIrrilevanti2) {
-            System.out.println("IRRELEVANT_2 : " + v.getName());
-            ap2[k++] = new AssignmentProposition(v, "True");
-            ap2[k++] = new AssignmentProposition(v, "False"); 
-                
+        AssignmentProposition[] ap2 = new AssignmentProposition[nodiIrrilevanti2.size() * 2];
+        int k = 0;
+        aus = 0;
+
+        System.out.println("NIRR 2: " + nodiIrrilevanti2.toString() + " - LEN: " + nodiIrrilevanti2.size());
+        for (RandomVariable v : nodiIrrilevanti2) {
+            System.out.println("IRRELEVANT_2 : " + v.getName() + " ****** " + aus);
+            aus++;
+            if (k < nodiIrrilevanti2.size() - 2) {
+                ap2[k++] = new AssignmentProposition(v, "Yes");
+                ap2[k++] = new AssignmentProposition(v, "No");
+            }
+
+            System.out.println("VAL INSERITI: " + ap2[k - 1]);
+            System.out.println("VAL INSERITI: " + ap2[k - 2]);
+
             //Elimino collegamento var irrilevanti -> figli e me le segno per ricalcolare i fattori
-            for(Node child : bn.getNode(v).getChildren()) {
+            for (Node child : bn.getNode(v).getChildren()) {
                 child.removeParent(bn.getNode(v));
                 nuoveCPT.add(child.getRandomVariable());
             }
         }
-        
-        //Ricalcolo le CPT delle var
-        for(RandomVariable c : nuoveCPT) {
-            Factor ff = makeFactor(c, ap2, bn);
-            bn.setNode(c, new FullCPTNode(c, ff.getValues(), bn.getNode(c).getParents().toArray(new Node[bn.getNode(c).getParents().size()])));
+        try {
+            //Ricalcolo le CPT delle var
+            for (RandomVariable c : nuoveCPT) {
+                Factor ff = makeFactor(c, ap2, bn);
+                bn.setNode(c, new FullCPTNode(c, ff.getValues(), bn.getNode(c).getParents().toArray(new Node[bn.getNode(c).getParents().size()])));
+            }
+        } catch (Exception e) {
+            System.out.println("RICALCOLO CPT ERROR: " + e.getMessage());
         }
-        
+
         /************************************************* Archi irrilevanti********************************************/
-        
-        for(AssignmentProposition ev : ap) {
+
+        for (AssignmentProposition ev : ap) {
             RandomVariable var = ev.getTermVariable();
-            for(Node c : bn.getNode(var).getChildren())
-            {
+            for (Node c : bn.getNode(var).getChildren()) {
                 Factor ff = makeFactor(c.getRandomVariable(), ap, bn);
                 bn.getNode(c.getRandomVariable()).removeParent(bn.getNode(var));
                 bn.setNode(c.getRandomVariable(), new FullCPTNode(c.getRandomVariable(), ff.getValues(), bn.getNode(c.getRandomVariable()).getParents().toArray(new Node[bn.getNode(c.getRandomVariable()).getParents().size()])));
@@ -113,7 +126,7 @@ public class NodiIrrilevanti {
         /******************************************* V.E. con ORDINAMENTI ************************************************************/
 
         // 1. MinDegree
-        EliminationAsk minDegreeOrderAsk = new EliminationAsk() {
+        /*EliminationAsk minDegreeOrderAsk = new EliminationAsk() {
             @Override
             protected List<RandomVariable> order(BayesianNetwork bn, Collection<RandomVariable> vars) {
                 return MinDegreeOrder.minDegreeOrder(bn);
@@ -138,75 +151,78 @@ public class NodiIrrilevanti {
         }
 
         System.out.println("MinDegreeOrder : " + MinDegreeOrder.minDegreeOrder(bn));
-        System.out.println("MinFillOrder : " + MinFillOrder.minFillOrder(bn));
+        System.out.println("MinFillOrder : " + MinFillOrder.minFillOrder(bn));*/
     }
-    
-    
+
+
 //**********************************************************************************************************
-    
+
     private static Factor makeFactor(RandomVariable var, AssignmentProposition[] e,
-                    BayesianNetwork bn) {
-
-        Node n = bn.getNode(var);
-        if (!(n instanceof FiniteNode)) {
+                                     BayesianNetwork bn) {
+        try {
+            Node n = bn.getNode(var);
+            if (!(n instanceof FiniteNode)) {
                 throw new IllegalArgumentException(
-                                "Elimination-Ask only works with finite Nodes.");
-        }
-        FiniteNode fn = (FiniteNode) n;
-        List<AssignmentProposition> evidence = new ArrayList<AssignmentProposition>();
-        for (AssignmentProposition ap : e) {
+                        "Elimination-Ask only works with finite Nodes.");
+            }
+            FiniteNode fn = (FiniteNode) n;
+            List<AssignmentProposition> evidence = new ArrayList<AssignmentProposition>();
+            for (AssignmentProposition ap : e) {
                 if (fn.getCPT().contains(ap.getTermVariable())) {
-                        evidence.add(ap);
+                    evidence.add(ap);
                 }
-        }
+            }
 
-        return fn.getCPT().getFactorFor(
-                            evidence.toArray(new AssignmentProposition[evidence.size()]));
+            return fn.getCPT().getFactorFor(
+                    evidence.toArray(new AssignmentProposition[evidence.size()]));
+        } catch (Exception e1) {
+            System.out.println("MAKE FACTOR ERROR: " + e1.getMessage());
+        }
+        return null;
     }
 
     /**********************************************************************************************************/
-    
+
     // Nodi Irrilevanti 1
     public static boolean isIrrelevant(BayesianNetwork bn, RandomVariable rnv, RandomVariable[] qrv, AssignmentProposition[] ap) {
         List<RandomVariable> query = Arrays.asList(qrv);
         List<AssignmentProposition> evidence = Arrays.asList(ap);
-              
-        for(RandomVariable q : query)
+
+        for (RandomVariable q : query)
             getAllAncestors(bn, q);
-        
-        for(AssignmentProposition e : evidence) 
+
+        for (AssignmentProposition e : evidence)
             getAllAncestors(bn, e.getTermVariable());
-        
-        if(ancestors.stream().filter(x -> x == rnv).findAny().isPresent() || 
-                query.stream().filter(x -> x == rnv).findAny().isPresent() || 
+
+        if (ancestors.stream().filter(x -> x == rnv).findAny().isPresent() ||
+                query.stream().filter(x -> x == rnv).findAny().isPresent() ||
                 evidence.stream().filter(x -> x.getTermVariable() == rnv).findAny().isPresent())
             return false;
-        else 
+        else
             return true;
     }
-        
-    public static void getAllAncestors(BayesianNetwork bn, RandomVariable rnv)
-    {
-        for(Node v : bn.getNode(rnv).getParents()) {
-            if(!ancestors.stream().filter(x -> x == v.getRandomVariable()).findAny().isPresent())
+
+    public static void getAllAncestors(BayesianNetwork bn, RandomVariable rnv) {
+        for (Node v : bn.getNode(rnv).getParents()) {
+            if (!ancestors.stream().filter(x -> x == v.getRandomVariable()).findAny().isPresent())
                 ancestors.add(v.getRandomVariable());
             getAllAncestors(bn, v.getRandomVariable());
         }
     }
-    
+
     /*******************************************************************************************************************************/
-    
+
     private static Graph<RandomVariable, DefaultEdge> graphFromBayesianNetwork(BayesianNetwork bayesianNetwork) {
         Graph<RandomVariable, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
 
         // aggiungo i nodi al grafo
-        for (RandomVariable randomVariable: bayesianNetwork.getVariablesInTopologicalOrder()) {
+        for (RandomVariable randomVariable : bayesianNetwork.getVariablesInTopologicalOrder()) {
 
             if (!graph.containsVertex(randomVariable)) {
                 graph.addVertex(randomVariable);
             }
 
-            for (Node child: bayesianNetwork.getNode(randomVariable).getChildren()) {
+            for (Node child : bayesianNetwork.getNode(randomVariable).getChildren()) {
                 RandomVariable rvChild = child.getRandomVariable();
                 if (!graph.containsVertex(rvChild)) {
                     graph.addVertex(rvChild);
@@ -219,9 +235,9 @@ public class NodiIrrilevanti {
     }
 
     private static void moralizeGraph(Graph<RandomVariable, DefaultEdge> graph, BayesianNetwork bn) {
-        for (RandomVariable randomVariable: graph.vertexSet()) {
+        for (RandomVariable randomVariable : graph.vertexSet()) {
             List<RandomVariable> parents = new ArrayList<>();
-            for (Node node: bn.getNode(randomVariable).getParents()) {
+            for (Node node : bn.getNode(randomVariable).getParents()) {
                 parents.add(node.getRandomVariable());
             }
 
@@ -242,11 +258,11 @@ public class NodiIrrilevanti {
         Graph<RandomVariable, DefaultEdge> graph = graphFromBayesianNetwork(bayesianNetwork);
         moralizeGraph(graph, bayesianNetwork);
 
-        for (RandomVariable evidence: evidences) {
+        for (RandomVariable evidence : evidences) {
             graph.removeVertex(evidence);
         }
 
-        for (RandomVariable query: queries) {
+        for (RandomVariable query : queries) {
             BreadthFirstIterator<RandomVariable, DefaultEdge> breadth = new BreadthFirstIterator<>(graph, query);
 
             Set<RandomVariable> path = new HashSet<>();
@@ -254,7 +270,7 @@ public class NodiIrrilevanti {
                 path.add(breadth.next());
             }
 
-            for (RandomVariable y: others) {
+            for (RandomVariable y : others) {
                 if (!path.contains(y)) {
                     irrilevants.add(y);
                 }
@@ -263,5 +279,5 @@ public class NodiIrrilevanti {
 
         return irrilevants;
     }
-    
+
 }
